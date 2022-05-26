0298843b4fc6eee1d0fa96a476fe12382ac89dd3
Hardik Parmar <hardik.parmar5@gmail.com>
2022-05-26 14:38:20 +0530

    seperating functions to a diffrent files/routing.

---
+++ helpers/queryHelpers.js
@@ -0,0 +1,61 @@
+//add element at any index
+
+const { data, newUser } = require("./dataForQuery");
+function addElement() {
+  let arr = data;
+  let index = 1;
+  let element = newUser;
+  for (i = arr.length; i > index; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[index] = element;
+}
+// addElement();
+// console.log(data);
+
+function firstAndLast() {
+  let arr = data;
+  let first = arr[0];
+  let last = arr[arr.length - 1];
+  console.log(first);
+  console.log(last);
+}
+// firstAndLast();
+
+function updateUserData(data, newUser) {
+  if (data.indexOf(newUser) === -1) {
+    console.log(data.indexOf(newUser));
+    data.push(newUser);
+    console.log("user updated");
+    console.log(data);
+  } else {
+    console.log("user exists");
+  }
+}
+// updateUserData(data, newUser);
+// updateUserData(data, newUser);
+
+module.exports = { updateUserData, firstAndLast, addElement };
+
+// add data at index
+// const addData = data.splice(5, 0, newUser);
+// console.log(data);
+
+//get first and last element from data
+// let first = data.at(0);
+// let last = data.at(-1);
+// console.log(first, last);
+//filtering data by name
+
+// const filteredUser = data.filter((obj) => {
+//   if (obj.name.first === "Carol") {
+//     return obj;
+//   }
+// });
+// const indexOfData = data.filter((obj) => {
+//   if (obj.name.first === "Indie") {
+//     return indexOf(obj);
+//   }
+// });
+// console.log(indexOfData);
+// console.log(filteredUser);
