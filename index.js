275da70326f6bbcd933c0a0b45c0944b9149928d
Hardik Parmar <hardik.parmar5@gmail.com>
2022-11-12 09:55:22 +0530

    Joi validation

--- index.js
+++ index.js
@@ -8,10 +8,113 @@ const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const { initializingPassport } = require("./helpers/passportHelper");
 const cron = require("node-cron");
+const Joi = require("joi");
 initializingPassport(passport);
 YAML = require("yamljs");
 require("dotenv").config();
 
+// const schema = Joi.string().min(10);
+// schema.validateAsync("ghosdfsdfqw");
+
+// const schema2 = Joi.object().keys({
+//   a: Joi.string().empty("").default("default value").lowercase(), //or we can use required()
+//   b: Joi.number(), //.integer() .limit() .nagative()
+//   c: Joi.boolean(),
+//   d: Joi.string().domain().required(),
+//   e: Joi.string().token(),
+// });
+// schema2.validateAsync({
+//   a: "sadasASDASDd",
+//   b: 1231,
+//   c: true,
+//   d: "www.google.com",
+// });
+// const schema3 = Joi.object({
+//   a: Joi.array().items(Joi.number()),
+//   b: Joi.number().valid(Joi.in("a")), //used to give array vales
+// });
+// schema3.validateAsync({
+//   a: [3, 44123, 342, 1324],
+//   b: 44123,
+// });
+
+// Joi.attempt("21", Joi.number());
+// const result = Joi.attempt("4", Joi.number());
+// console.log({ result });
+
+// const schema4 = Joi.object({
+//   a: Joi.ref("b.c"),
+//   b: {
+//     c: Joi.any(),
+//   },
+//   c: Joi.ref("$x"),
+// });
+
+// schema4.validateAsync({ a: 5, b: { c: 5 } }, { c: { x: 5 } });
+
+// //define type so we dont have to use as as functions
+
+// const { object, string } = Joi.types();
+// const schema5 = object.keys({
+//   property: string.min(4),
+// });
+// const schema6 = Joi.object({
+//   a: Joi.number()
+//     .min(0)
+//     .error((errors) => new Error("requires a positive number")),
+// });
+// schema6.validateAsync({ a: 1 });
+// const schema7 = Joi.object({
+//   min: Joi.number(),
+//   max: Joi.number().when("min", {
+//     is: Joi.number().required(),
+//     then: Joi.number().greater(Joi.ref("min")),
+//   }),
+// });
+// schema7.validateAsync({
+//   min: 5,
+//   max: 6,
+// });
+
+// //array length and max and min values inside array
+// const schema8 = Joi.array().items(Joi.string(), Joi.number()).length(2).max(2).min(1).unique();
+// const schema11 = Joi.array().sort({
+//   order: string,
+// });
+// schema8.validateAsync([1, "ASdasd"]);
+
+// const schema9 = Joi.date().greater("1-1-1974"); // .less and .max and .min // .isoDate(), isoDuration()
+// const schema10 = Joi.date().greater("now"); //for current date
+// const schema12 = Joi.object({
+//   a: Joi.any(),
+//   b: Joi.any(),
+// }).and("a", "b"); // a and b both are required if any one of them is available
+// const schema13 = Joi.object().instance(schema12); //refrence
+// const schema14 = Joi.object().length(5).max(5).min(3);
+
+// const schema15 = Joi.object({
+//   a: Joi.any(),
+//   b: Joi.any(),
+// }).nand("a", "b"); //not all should be present. in this case all can be empbty
+
+// const schema16 = Joi.object({
+//   a: Joi.any(),
+//   b: Joi.any(),
+// }).or("a", "b"); //same as nand but atleast 1 is needed.
+
+// const schema17 = Joi.object({
+//   a: Joi.any(),
+//   b: Joi.any(),
+// }).with("a", "b"); //  if there is a then b is required.
+
+// const schema18 = Joi.object({
+//   a: Joi.any(),
+//   b: Joi.any(),
+// }).without("a", ["b"]); //if there is a then b is not allowed.
+
+// const schema19 = Joi.string().guid({
+//   version: ["uuidv4", "uuidv5"],
+// }); // for uuid and others are .hostname , .ip for ip address, .hex() for hexa decimal string
 //cron working every minute
 
 // cron.schedule("* * * * *", () => {
